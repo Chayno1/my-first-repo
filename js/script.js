efb06dfb133e4e5bc2092038719189b5b9e7d4bf
Chayno1 <chazmaster71@hotmail.co.uk>
2021-09-15 22:00:29 +0000

    far as i can go

--- js/script.js
+++ js/script.js
@@ -1,7 +1,51 @@
-//let rectangles = document.getElementsByClassName("cell-1");
-//console.log(rectangles);
+document.addEventListener("DOMContentLoaded", function() {
 
-let house = [cars, bikes];
+});
 
-console.log(house);
+let running = false;
+let startButton = document.getElementById("start-game");
+
+startButton.addEventListener('click', function() {
+    if (running) {
+        running = false
+        changeColors()
+    } else {
+        running = true
+        changeColors()
+    }
+})
+
+runGame()
+
+DifficultSetting()
+
+calculateCorrectAnswer()
+
+revomeVisibilty()
+
+checkAnswer()
+
+incrementScore()
+
+incrementWrongScore()
+
+endGame()
+
+let colors = [ 'blue', 'purple', 'green', 'gold', 'orange', 'red'];
+let recs = document.getElementsByClassName("cells");
+let totalColors = colors.length;
+let rand = Math.random();
+let randIndex = Math.floor(rand * totalColors);
+let randomColor = colors[randIndex];
+
+
+function changeColors() {
+    if (running) {
+        recs.forEach(color =>{
+        color.style.background = randomColor;
+    })
+    }
+    }
+
+previousScore()
 
